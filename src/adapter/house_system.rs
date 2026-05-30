ac7f20b234f7cb650baab74c1227f028aec3e421
Sawfan <sawfan@sawfan.com>
2026-05-29 21:36:25 -0500

    Adds house and zodiac systems

---
+++ src/adapter/house_system.rs
@@ -0,0 +1,17 @@
+pub fn house_system_to_swisseph(system: rubrum::HouseSystem) -> swisseph::HouseSystemKind {
+    match system {
+        rubrum::HouseSystem::Placidus => swisseph::HouseSystemKind::Placidus,
+        rubrum::HouseSystem::Koch => swisseph::HouseSystemKind::Koch,
+        rubrum::HouseSystem::Porphyry => swisseph::HouseSystemKind::Porphyrius,
+        rubrum::HouseSystem::Regiomontanus => swisseph::HouseSystemKind::Regiomontanus,
+        rubrum::HouseSystem::Campanus => swisseph::HouseSystemKind::Campanus,
+        rubrum::HouseSystem::Equal => swisseph::HouseSystemKind::Equal,
+        rubrum::HouseSystem::WholeSign => swisseph::HouseSystemKind::WholeSign,
+        rubrum::HouseSystem::Meridian => {
+            swisseph::HouseSystemKind::AxialRotationSystemMeridianSystemZariel
+        }
+        rubrum::HouseSystem::Alcabitius => swisseph::HouseSystemKind::Alcabitus,
+        rubrum::HouseSystem::Morinus => swisseph::HouseSystemKind::Morinus,
+        rubrum::HouseSystem::Topocentric => swisseph::HouseSystemKind::PolichPageTopocentricSystem,
+    }
+}
